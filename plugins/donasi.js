d42a6f239914443be1eb5626a7c4286e86f03d11
IRJA <93579297+irja333@users.noreply.github.com>
2022-05-14 01:00:58 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,7 +1,6 @@
 let handler =  m => m.reply(`
-╭─「 Group 」
-│ • https://chat.whatsapp.com/Fd92L258maQ6lWjIua6Djq
-╰────
+「 Group 」
+https://chat.whatsapp.com/Fd92L258maQ6lWjIua6Djq
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
 handler.tags = ['info']
